932a1cff9b76dfea5004fe09bd752ecb04acecfe
winstonwang <wwang@lcs.on.ca>
2022-05-11 11:31:29 -0400

    Initial Commit

---
+++ Culminating/CulminatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CulminatingApp.swift
+//  Culminating
+//
+//  Created by Winston Wang on 2022-05-11.
+//
+
+import SwiftUI
+
+@main
+struct CulminatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
